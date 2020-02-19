b488bc71698b0569f911e5aa2095f95d983bc5eb
JuliamCosta <juuhcosta@outlook.com>
2020-02-19 17:29:21 -0300

    sem bootstrap

--- index.php
+++ index.php
@@ -2,26 +2,13 @@
 <html lang="pt-br">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <title>Atividades da Julia</title>    	
-        <link rel="stylesheet" href="css/bootstrap.min.css">
-        <link rel="stylesheet" href="css/bootstrap-grid.css">
-        <link rel="stylesheet" href="css/bootstrap-grid.min.css">
-        <link rel="stylesheet" href="css/bootstrap-reboot.css">
-        <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
-        <link rel="stylesheet" href="css/bootstrap.css">
     </head>
 
     <body>
         
         <div>
-            <h1 class="display-4 font-italic font-weight-normal text-white text-center">Links das Minhas Atividades</h1>
+            <h1>Links das Minhas Atividades</h1>
         </div>
-		<div class="bg-light"></div>
-		
-        <script src="js/bootstrap.bundle.js"></script>
-        <script src="js/bootstrap.bundle.min.js"></script>
-        <script src="js/bootstrap.js"></script>
-        <script src="js/bootstrap.min.js"></script>
     </body>
 </html>
