e710d70b3d77df7fd78461c1d49d205d4c9bcc2b
JuliamCosta <juuhcosta@outlook.com>
2020-09-16 15:42:32 -0300

    Index 2020.09.16

--- index.php
+++ index.php
@@ -6,13 +6,13 @@
     </head>
 
     <body>
-        
         <div>
             <h1>Links das Minhas Atividades</h1>
         </div>
 		 <a href="20200811/atividade1.php">Editor de Texto</a><br>
          <a href="20200819/atividade.php">Engenharia Reversa - Jquery</a><br>
          <a href="20200825/atividade.php">Engenharia Reversa - Lista Frutas</a><br>
+         <a href="20200826/index.php">Exercicio Compartilhado - Editor de Texto</a><br>
          <a href="20200902/index.php">API ViaCEP</a><br>
          <a href="20200916/index.php">Trabalho Prático Bimestral</a>
     </body>
